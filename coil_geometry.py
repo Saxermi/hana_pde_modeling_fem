67c962adfc77e839c37635bd4ab96ed470711fe2
SamuLibero <57075720+SamuLibero@users.noreply.github.com>
2025-01-03 18:32:31 +0100

    Addded create_homo_geometry_cube for position visualisation

--- coil_geometry.py
+++ coil_geometry.py
@@ -77,6 +77,66 @@ def create_homo_geometry(nwindings=6, wireradius=0.001, coilradius=0.01):
     coil.faces.maxh = wireradius
     coil.solids.name = 'coil'
     coil.name = 'coil'
+        
+    crosssection = coil.faces.Max(Z).mass
+
+    # Create the surrounding air box
+    box = Box((-airh / 2, -airh / 2, -airh / 2), (airh / 2, airh / 2, airh / 2))
+    box.faces.name = "outer"
+
+    # Subtract the coil from the air box to form the air region
+    air = box - coil
+    air.mat("air")
+
+    # Combine the coil and air region into a single geometry
+    geo = OCCGeometry(Glue([coil, air, cube]))
+
+    return geo, crosssection
+
+
+def create_homo_geometry_cube(nwindings=6, wireradius=0.001, coilradius=0.01, center_x=0, center_y=0, center_z=0):
+    """
+    Create a homogeneous coil geometry.
+
+    Parameters:
+    nwindings (int): Number of windings in the coil. Default is 6.
+    wireradius (float): Radius of the wire used in the coil. Default is 0.001.
+    coilradius (float): Radius of the coil. Default is 0.01.
+
+    Returns:
+    OCCGeometry: The combined geometry of the coil and the surrounding air region.
+    crosssection: 
+    """
+    coilheight = nwindings * (2 * wireradius) * 1.2
+    airh = 10 * coilradius
+
+    # Create the coil geometry using cylinders
+    cyl1 = Cylinder((0, 0, -coilheight / 2), Z, r=coilradius + wireradius, h=coilheight)
+    cyl2 = Cylinder((0, 0, -coilheight / 2), Z, r=coilradius - wireradius, h=coilheight)
+
+    # Subtract the inner cylinder from the outer cylinder to form the coil
+    coil = cyl1 - cyl2
+
+    # Set mesh size for the coil
+    coil.solids.maxh = wireradius
+    coil.edges.maxh = wireradius
+    coil.faces.maxh = wireradius
+    coil.solids.name = 'coil'
+    coil.name = 'coil'
+    
+    # Dimensions
+    width, height, depth = 0.001, 0.001, 0.001
+
+    # Calculate the corner points based on the center and dimensions
+    corner1 = (center_x - width / 2, center_y - height / 2, center_z - depth / 2)
+    corner2 = (center_x + width / 2, center_y + height / 2, center_z + depth / 2)
+
+    # Create a box using the calculated corner points
+    cube = Box(Pnt(*corner1), Pnt(*corner2))
+    cube.mat("cube")
+    cube.faces.name = "cubebnd"
+    cube.solids.name = 'coil'
+    cube.name = 'coil'
     
     crosssection = coil.faces.Max(Z).mass
 
@@ -89,6 +149,6 @@ def create_homo_geometry(nwindings=6, wireradius=0.001, coilradius=0.01):
     air.mat("air")
 
     # Combine the coil and air region into a single geometry
-    geo = OCCGeometry(Glue([coil, air]))
+    geo = OCCGeometry(Glue([coil, air, cube]))
 
     return geo, crosssection
